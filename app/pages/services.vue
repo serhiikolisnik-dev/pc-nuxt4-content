468668fda23bcdd342964d4ea1aee120a7f0a372
Serhii.Kolisnyk <serhii.kolisnyk@powercode.co.uk>
2025-10-02 18:52:44 +0300

    test

--- app/pages/services.vue
+++ app/pages/services.vue
@@ -1,15 +1,19 @@
 <script setup lang="ts">
-const { data: services } = await useAsyncData('services', () => {
-  return queryCollection('services')
-    .order('id', 'DESC')
-    .all()
-})
+// const { data: services } = await useAsyncData('services', () => {
+//   return queryCollection('services')
+//     .order('id', 'DESC')
+//     .all()
+// })
+
+const data = await useAsyncData(() => queryCollection('services')
+
+// const { data: services } = await useAsyncData('services', () => )
 </script>
 
 <template>
 <!--  <ContentRenderer v-if="services" :value="services" />-->
-  <div v-if="services">
-    {{services}}
+  <div v-if="data">
+    {{data}}
   </div>
 
   <div v-else>Services not found</div>
