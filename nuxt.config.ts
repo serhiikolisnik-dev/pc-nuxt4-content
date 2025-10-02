56e5ee1bf2eee8e45918c483013a98bd41ab3f72
Serhii.Kolisnyk <serhii.kolisnyk@powercode.co.uk>
2025-10-02 17:46:10 +0300

    test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  typescript: {
+    typeCheck: true,
+  },
 
   modules: [
     '@nuxt/content',
