57827d8100a08cfb7dd3a1a0559f68e71e15fe46
Serhii.Kolisnyk <serhii.kolisnyk@powercode.co.uk>
2025-10-02 18:25:17 +0300

    test

---
+++ app/pages/services.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const { data: services } = await useAsyncData(() => queryCollection('content').path('/services').first())
+
+useSeoMeta({
+  title: services.value?.title,
+  description: services.value?.description
+})
+</script>
+
+<template>
+<!--  <ContentRenderer v-if="services" :value="services" />-->
+  <div v-if="services">
+    {{services}}
+  </div>
+
+  <div v-else>Services not found</div>
+</template>
