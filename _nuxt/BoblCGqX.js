9d07b38591aa164c8c4e502b2d55ebb45e5abcae
SergeyKolesnikDev <92144945+SergeyKolesnikDev@users.noreply.github.com>
2025-10-03 08:28:55 +0000

    Deploying to gh-pages from @ serhiikolisnik-dev/pc-nuxt4-content@911d0dd7b2228c24a6c5e639f0a63eef9a3f69c1 🚀

--- _nuxt/BoblCGqX.js
+++
@@ -1,1 +0,0 @@
-import{_ as a}from"./BND7ctRX.js";import{N as n,i as s,w as f,O as p}from"#entry";const i={__name:"ProseA",props:{href:{type:String,default:""},target:{type:String,default:void 0,required:!1}},setup(e){const t=e;return(r,_)=>{const o=a;return s(),n(o,{href:t.href,target:t.target},{default:f(()=>[p(r.$slots,"default")]),_:3},8,["href","target"])}}};export{i as default};
